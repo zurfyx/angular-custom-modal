cc2083df01055380eda4cfa6111697fa8ffb2d9c
zurfyx <zurfyx@gmail.com>
2017-10-27 12:40:14 +0200

    complete demo

--- example/app/app.component.ts
+++ example/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { ModalComponent } from '../../src';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('componentInsideModal') componentInsideModal: ModalComponent;
+
+  openFromComponent() {
+    this.componentInsideModal.open();
+  }
 }
