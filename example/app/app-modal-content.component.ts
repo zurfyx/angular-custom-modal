00cd280ac709688a37bbe66ac4c3597881111070
zurfyx <zurfyx@gmail.com>
2017-10-30 10:17:58 +0100

    console.info ngOnInit ngOnDestroy

--- example/app/app-modal-content.component.ts
+++ example/app/app-modal-content.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-modal-content',
   templateUrl: 'app-modal-content.component.html',
 })
-export class AppModalContentComponent implements OnInit {
+export class AppModalContentComponent implements OnInit, OnDestroy {
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    /* tslint:disable */
+    console.info('AppModalContentComponent: created');
+    /* tslint:enable */
+  }
+
+  ngOnDestroy() {
+    /* tslint:disable */
+    console.info('AppModalContentComponent: destroyed');
+    /* tslint:enable */
+  }
 }
